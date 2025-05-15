52a9c24f5b83df0f26f5e1fb9c04481073ff0783
Rana Aldosari <rana@example.com>
2025-05-15 13:52:49 +0300

    done

---
+++ main.js
@@ -0,0 +1,151 @@
+
+
+let root=document.getElementById("root")
+let element=
+<div>
+<h2 className="container">Rana Aldosari</h2>
+<h4 className="container">Front-End Developer</h4>
+<h6 className="title">About</h6>
+<hr></hr>
+<p className="textAbout">A passionate and motivated front-end developer, eager to develop my skills in creating dynamic and interactive websites
+using HTML, CSS, JavaScript, and React.js. I’m dedicated to enhancing user interfaces and experiences with modern web
+technologies. and eager to join a team where I can further develop my skills and contribute to meaningful projects.
+</p>
+<h6 className="title">Education</h6>
+<hr></hr>
+<div className="contentContainer">
+<p>
+Bachelor’s Computer Science ,Prince Sattam bin Abdulaziz University.
+</p>
+<p>Aug 2020 – Aug 2024</p>
+</div>
+<p>• Relevant coursework includes Web Development, Database Systems, Machine Learning, Data Structures, Algorithms.
+GPA: 4.86 / 5</p>
+<h6 className="title">Experience</h6>
+<hr></hr>
+<div className="contentContainer">
+<h5><b>COOP Training</b> ,King Abdulaziz City for Science and Technology (KACST).
+</h5>
+<p>June 2023 – Aug 2023
+</p>
+</div>
+<p>• During my COOP training at KACST, I had the opportunity to work in the Computer Science department, where I gained
+valuable experience in several key areas, including UI/UX design, software and web development, cybersecurity, and
+networking.
+</p>
+<div className="contentContainer">
+<h5><b>Web Development</b> ,Hash plus Camp.
+</h5>
+<p>July 2024 – Aug 2024
+</p>
+</div>
+<p>• During my train at Hash Camp, I gained valuable experience in software development, focusing on web design and
+development. I learned how to create dynamic and interactive websites using HTML, CSS, and JavaScript. I designed an
+attractive and responsive login page using JavaScript, which enhanced my skills in creating dynamic user interfaces and
+improving the user experience.
+</p>
+
+<div className="contentContainer">
+<h5><b>Build AI Application</b> ,SDAIA
+</h5>
+<p>OCT 2024 – NOV 2024
+</p>
+</div>
+<p></p>
+
+<h5 className="title">Projects</h5>
+<hr></hr>
+<div className="contentContainer">
+<h5><b>Fusion OCR </b> , (Graduation project)
+</h5>
+</div>
+<p>• A mobile application designed for recognizing handwriting and converting it into a digital format, Aiming to bridge the gap
+between traditional handwriting and the digital world. It utilizes Deep Learning Technologies and Artificial Intelligence to
+create a robust recognition system. The user is allowed to capture, edit, and store text.
+</p>
+
+<div className="contentContainer">
+<h5><b>TO-DO-List </b> 
+</h5>
+</div>
+<p>• An interactive website aimed at managing tasks, It is created using HTML CSS and JavaScript ,The user can write, edit and
+delete a task after completing it.
+</p>
+
+<div className="contentContainer">
+<h5><b>Prayer Times
+</b> 
+</h5>
+</div>
+<p>• An interactive website to display prayer times according to cities, The times change when the user changes the city, using
+HTML CSS JavaScript and API
+</p>
+
+<div className="contentContainer">
+<h5><b>Get Along With Me Website ,(volunteer)
+</b> 
+</h5>
+</div>
+<p>• In this project, I developed a website using HTML, CSS, JavaScript, and React. I collaborated closely with a dedicated team
+of volunteers and stakeholders to understand their vision and needs. This experience enhanced my technical skills and
+fostered effective communication and teamwork in a dynamic environment.
+</p>
+
+<h5 className="title">Certifications</h5>
+<hr></hr>
+<div className="contentContainer">
+<h5><b>The Complete 2024 Web Development Bootcamp, Udemy 
+</b> 
+</h5>
+<p>61.5 Hours</p>
+</div>
+
+<div className="contentContainer">
+<h5><b>Full-Stack JavaScript Path, Satr
+</b> 
+</h5>
+<p>26 Hours</p>
+</div>
+
+<h5 className="title">Hard skills</h5>
+<hr></hr>
+<div className="listItem">
+<p>Figma</p>
+<p>HTML/CSS</p>
+<p>Bootstrap</p>
+<p>Tailwind</p>
+<p>JavaScript</p>
+</div>
+<div className="listItem">
+<p>Node.js </p>
+<p>Rest APIs </p>
+<p>React js </p>
+<p>Next.js </p>
+<p>Github</p>
+</div>
+<h5 className="title">Soft skills</h5>
+<hr></hr>
+<div className="listItem">
+<p>Teamwork</p>
+<p>Flexibility</p>
+<p>Critical Thinking </p>
+<p>Problems solving </p>
+</div>
+<div className="listItem">
+<p>Organizing and planning </p>
+<p>Communication skills </p>
+<p>Attention to detail </p>
+
+<p>Time management</p>
+</div>
+<h5 className="title">Languages</h5>
+<hr></hr>
+<h5><b>Arabic, Native
+</b> 
+</h5>
+<h5><b>English, Advance
+</b> 
+</h5>
+</div>
+ReactDOM.render(element,root)
+
